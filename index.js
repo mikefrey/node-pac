615c24d53d56ebf2f11826db0d68d9f781845360
Mike Frey <mike.frey@tstmedia.com>
2013-06-02 22:21:58 -0500

    first pass

---
+++ index.js
@@ -0,0 +1,107 @@
+var fs = require('fs')
+var tgz = require('tar.gz')
+var mkdirp = require('mkdirp')
+var Path = require('path')
+var glob = require('glob')
+var semver = require('semver')
+
+var pkgjson = require(Path.join(__dirname, 'package.json'))
+var modulePath = Path.join(__dirname, '.modules')
+
+var sep = '@'
+
+// ensure that the .modules directory exists
+mkdirp.sync(modulePath)
+
+
+var log = function() {
+  console.log.apply(console, arguments)
+}
+
+var error = function() {
+  console.error.apply(console, arguments)
+}
+
+var tryRequire = function(p) {
+  try {
+    return require(p)
+  } catch(ex) {}
+  return null
+}
+
+var pack = function(name, version) {
+  log('Packing', name+sep+version)
+  var source = Path.join(__dirname, 'node_modules', name)
+  var dest = Path.join(modulePath, name+sep+version+'.tgz')
+  new targz().compress(source, dest, function(err) {
+    if (err)
+      error('Failed to pack', name)
+    else
+      log('Packed', name)
+  }
+}
+
+
+var filesToHash = function(files) {
+  return curFiles.reduce(function(memo, file) {
+  var f = file.replace(/\.tgz$/i, '').split(sep)
+  memo[f[0]] = f[1]
+  return memo
+}, {})
+}
+
+// get a list of all the currently created files
+var curFiles = glob.sync('*.tgz', {cwd:modulePath})
+
+// separate the file list into a hash of name/version
+var curMods = filesToHash()
+
+// get dependency list
+var deps = pkgjson.dependencies
+
+// remove any packed modules that aren't needed
+// or don't meet version requirements
+Object.keys(curMods).forEach(function(name) {
+  if (!deps[name] || !semver.satisfies(curMods[name], deps[name])) {
+    var fv = name+sep+curMods[name]
+    log('Removing ', fv)
+    fs.unlinkSync(Path.join(modulePath, fv))
+    delete curMods[name]
+  }
+})
+
+
+
+// figure out what modules need packing up and pack them
+Object.keys(deps).forEach(function(name) {
+  var needs = deps[name]
+  if (!curMods[name]) {
+    var pkg = tryRequire(Path.join('./node_modules', name, 'package.json'))
+    if (pkg && semver.satisfies(pkg.version, needs]) {
+      pack(name, pkg.version)
+    } else {
+      error('Unmet dependency', name+sep+needs)
+    }
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gzip = zlib.createGzip()
+var inp = fs.createReadStream('input.txt')
+var out = fs.createWriteStream('input.txt.gz')
+
+inp.pipe(gzip).pipe(out);
+
