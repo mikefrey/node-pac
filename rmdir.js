a2cff593775249a7e55d5ab17192212a7d47c089
Darcy Murphy <mrDarcyMurphy@gmail.com>
2013-06-05 18:21:47 -0500

    add recursive and synchronous rmdir

---
+++ rmdir.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+
+var rmdir = module.exports = function(dirPath) {
+  var files = fs.readdirSync(dirPath)
+  // try { var files = fs.readdirSync(dirPath) }
+  // catch(e) { return }
+  if (files.length > 0)
+    for (var i = 0; i < files.length; i++) {
+      var filePath = dirPath + '/' + files[i]
+      if (fs.statSync(filePath).isFile())
+        fs.unlinkSync(filePath)
+      else
+        rmdir(filePath)
+    }
+  fs.rmdirSync(dirPath)
+}
